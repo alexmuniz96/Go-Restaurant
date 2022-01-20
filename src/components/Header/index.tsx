9bbda4724d4ecc54b01af9739d248a5ec36fe5a5
alexmuniz96 <alexmuniz096@gmail.com>
2022-01-19 22:11:04 -0300

    Header refatorado para TS e functions components

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,32 @@
+import { Component } from 'react';
+import { FiPlusSquare } from 'react-icons/fi';
+
+import { Container } from './styles';
+import Logo from '../../assets/logo.svg';
+
+interface PropsHeader {
+  openModal: () => void,
+}
+
+export default function Header({ openModal }: PropsHeader) {
+  return (
+    <Container>
+      <header>
+        <img src={Logo} alt="GoRestaurant" />
+        <nav>
+          <div>
+            <button
+              type="button"
+              onClick={openModal}
+            >
+              <div className="text">Novo Prato</div>
+              <div className="icon">
+                <FiPlusSquare size={24} />
+              </div>
+            </button>
+          </div>
+        </nav>
+      </header>
+    </Container>
+  )
+}
